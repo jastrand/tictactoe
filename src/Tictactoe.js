6169629611281dfb789f6dac3629c7f94d8ac5c1
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-04-08 15:14:52 +0200

    build the game, missing styling

---
+++ src/Tictactoe.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./tictactoe.css";
+
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>{props.value}</button>
+  )
+}
+
+const Board = () => {
+  const [boardSquares, setBoardSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const handleClick = index => {
+
+    const squares = [...boardSquares]
+
+    if (calulateWinner(boardSquares) || squares[index]) return;
+
+    squares[index] = xIsNext ? "X" : "O"
+
+    setBoardSquares(squares)
+
+    setXIsNext(!xIsNext)
+  }
+
+  const renderSquare = index => {
+    return (
+      <Square value={boardSquares[index]} onClick={() => handleClick(index)} />
+    )
+  }
+
+  let status;
+  const winner = calulateWinner(boardSquares)
+  status = winner
+    ? `Winner is ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`
+
+  return (
+    <div className="board">
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+const calulateWinner = (squares) => {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c])
+      return squares[a]
+  }
+  return null;
+}
+
+
+
+export const Tictactoe = () => {
+  return (
+    <div>
+      <Board />
+    </div>
+  )
+};
